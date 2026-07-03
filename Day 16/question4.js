27b5fa6e5f9a48f97579a1bfd61b3083560d0fec
codeuv30 <codeuv30@gmail.com>
2026-07-03 20:00:24 +0530

    DSA: DAY 16: QUESTION 4 SOLVED

---
+++ Day 16/question4.js
@@ -0,0 +1,22 @@
+/* QUESTION */
+/* TAKE A NUMBER FROM USER */
+/* LET'S SAY YOU GET NUMBER 5 THEN YOU HAVE TO PRINT RIGHT ANGLE TRIANGLE BUT ITS INVERTED */
+/* 
+1) * * * * *
+2) * * * *
+3) * * *
+4) * *
+5) *
+*/
+/* MEANS YOU HAVE TO PRINT THIS PATTERN WITH THE NUMBER OF ROWS YOU GET FROM USER */
+
+/* ANSWER */
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter a number: "));
+
+for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= n - i + 1; j++) {
+        process.stdout.write("* ")
+    }
+    console.log();
+}
