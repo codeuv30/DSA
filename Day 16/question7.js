cbd87de93a83a6eed6ba86eeb4f002a8af84d2aa
codeuv30 <codeuv30@gmail.com>
2026-07-03 22:37:39 +0530

    DSA: DAY 16: QUESTION 7 SOLVED

---
+++ Day 16/question7.js
@@ -0,0 +1,26 @@
+/* QUESTION */
+/* TAKE A NUMBER FROM USER */
+/* LET'S SAY YOU GET NUMBER 5 THEN YOU HAVE TO PRINT A PATTERN*/
+/*
+*   *
+ * * 
+  *  
+ * * 
+*   *
+ */
+/* MEANS YOU HAVE TO PRINT THIS PATTERN WITH THE NUMBER OF ROWS YOU GET FROM USER */
+
+/* ANSWER */
+
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter a number: "));
+
+for(let i = 1; i<=n; i++) {
+    for(let j = 1; j<=n; j++) {
+        if(i === j) process.stdout.write("*");
+        else if((i + j) === n+1) process.stdout.write("*");
+        else process.stdout.write(" ");
+    }
+    
+    console.log();
+}
