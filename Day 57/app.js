0e87c31dc351aba86d22cb115e31e9f60e314c83
codeuv30 <codeuv30@gmail.com>
2026-07-31 17:27:15 +0530

    DSA: DAY 57: QUETION 1 SOLVED SELF

--- Day 57/app.js
+++ Day 57/app.js
@@ -1,32 +1,33 @@
 function divide(arr, first, last) {
-
-    if(first >= last) return;
+    if( first >= last) return;
 
     let mid = Math.floor((first + last) / 2);
 
     divide(arr, first, mid);
-    divide(arr, mid + 1, last);
+    divide(arr, mid +1 , last);
 
     merge(arr, first, mid, last);
-
 }
 
 function merge(arr, first, mid, last) {
     let temp = new Array(last - first + 1);
 
-    let i = first, j = mid + 1, k = 0;
+    let i = first;
+    let j = mid + 1;
+    let k = 0;
 
     while(i <= mid && j <= last) {
         if(arr[i] < arr[j]) temp[k++] = arr[i++];
         else temp[k++] = arr[j++];
-    }
+    };
 
     while(i <= mid) temp[k++] = arr[i++];
     while(j <= last) temp[k++] = arr[j++];
 
-    i = first, k = 0;
+    i = first;
+    k = 0;
 
-    while(k < temp.length) arr[i++] = temp[k++];
+    while(k < temp.length) arr[i++] = temp[k++]
 }
 
 let arr = [10, 5, 1, 9, 14, 6, 19, 12];
